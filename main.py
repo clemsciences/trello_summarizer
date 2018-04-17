b1f34847dded78b6eed41ae04da869fcc6b6a019
clemsciences <clemsciences@aol.com>
2018-04-17 23:46:52 +0200

    Il faut faire json.loads pour traiter la réponse de l'API, puis on fouille les données.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 import requests
+import json
 with open("key", "r") as f:
     key = f.read().strip()
 with open("token", "r") as f:
@@ -8,5 +9,23 @@ with open("token", "r") as f:
 param = {"key": key, "token": token}
 
 board_id = "5a250da362e909d26e2c541b"
-b = requests.request("GET", "https://api.trello.com/1/boards/"+board_id, params=param)
-print(b.text)
+b = json.loads(requests.request("GET", "https://api.trello.com/1/boards/"+board_id, params=param).text)
+print(b)
+# print(b.text)
+# print(b.text)
+# titre = b.text["name"]
+# Pour chaque utilisateur, faire la liste des choses qu'il a faites
+
+# https://api.trello.com/1/boards/id/memberships
+memberships = json.loads(requests.request("GET", "https://api.trello.com/1/boards/"+board_id+"/memberships", params=param).text)
+# https://api.trello.com/1/boards/id/cards
+cards = json.loads(requests.request("GET", "https://api.trello.com/1/boards/"+board_id+"/cards", params=param).text)
+print(cards)
+for card in cards:
+    print(card)
+    c = json.loads(requests.request("GET", "https://api.trello.com/1/cards/"+card["id"], params=param).text)
+    print(c)
+# response = requests.request("GET", url, params=param)
+
+# print(response.text)
+
