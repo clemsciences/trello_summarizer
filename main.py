4f6d966629ca46e755c5af7c7d43de9151b8bcb4
clement_besnier <clemsciences@aol.com>
2018-04-20 17:41:35 +0200

    Ordering actions

--- main.py
+++ main.py
@@ -21,23 +21,36 @@ memberships = json.loads(requests.request("GET", "https://api.trello.com/1/board
 # https://api.trello.com/1/boards/id/cards
 cards = json.loads(requests.request("GET", "https://api.trello.com/1/boards/"+board_id+"/cards", params=param).text)
 # print(cards)
+l_titres = []
+l_dates = []
+l_textes = []
+l_descr = []
 for card in cards:
     print("-------------------------")
-    print(card)
+    # print(card)
     c = json.loads(requests.request("GET", "https://api.trello.com/1/cards/"+card["id"], params=param).text)
     actions = json.loads(requests.request("GET", "https://api.trello.com/1/cards/"+card["id"]+"/actions", params=param).text)
-    print("Nom de la carte : ", c["name"])
-    print("Description : ", c["desc"])
+    # print("Nom de la carte : ", c["name"])
+    l_titres.append(c["name"])
+    # print("Description : ", c["desc"])
+    l_descr.append(c["desc"])
     # print("action : ", actions)
     for action in actions:
-        print(action)
-        print("\ndata :", action["data"])
+        # print(action)
+        # print("\ndata :", action["data"])
         if "text" in action["data"]:
-            print("action : ", action["data"]["text"])
-            print("date : ", action["date"])
-        else:
-            print(action)
+            # print("action : ", action["data"]["text"])
+            # print("date : ", action["date"])
+            l_dates.append(action["date"])
+            l_textes.append(action["data"]["text"])
+        # else:
+        #     print(action)
 # response = requests.request("GET", url, params=param)
 
 # print(response.text)
+
+print(l_titres)
+print(l_dates)
+print(l_textes)
+print(l_descr)
 
