544ed975d07a9113b0e2facc3c2ecba2c106f8c1
clemsciences <clemsciences@aol.com>
2018-04-23 00:04:38 +0200

    Class apparition

--- main.py
+++ main.py
@@ -1,8 +1,43 @@
 
 import requests
 import json
+
+
+class Action:
+    def __init__(self):
+        self.date = None
+        self.texte = ""
+
+    def from_action(self, act):
+        self.date = act["date"]
+        self.texte = act["data"]["text"]
+
+
+class Carte:
+    def __init__(self):
+        self.description = ""
+        self.titre = ""
+
+    def from_card(self, c):
+        self.description = c["desc"]
+        self.titre = c["name"]
+
+
+class Necessaire:
+    def __init__(self):
+        self.l_actions = []
+        self.l_titres = []
+        self.l_dates = []
+        self.l_textes = []
+        self.l_descr = []
+
+    def from_action(self, action):
+        self.l_actions.append(Action(action))
+
+
 with open("key", "r") as f:
     key = f.read().strip()
+
 with open("token", "r") as f:
     token = f.read().strip()
 
@@ -11,6 +46,7 @@ param = {"key": key, "token": token}
 board_id = "5a250da362e909d26e2c541b"
 b = json.loads(requests.request("GET", "https://api.trello.com/1/boards/"+board_id, params=param).text)
 print(b)
+
 # print(b.text)
 # print(b.text)
 # titre = b.text["name"]
@@ -21,19 +57,24 @@ memberships = json.loads(requests.request("GET", "https://api.trello.com/1/board
 # https://api.trello.com/1/boards/id/cards
 cards = json.loads(requests.request("GET", "https://api.trello.com/1/boards/"+board_id+"/cards", params=param).text)
 # print(cards)
-l_titres = []
-l_dates = []
-l_textes = []
-l_descr = []
+# l_titres = []
+# l_dates = []
+# l_textes = []
+# l_descr = []
+l = []
 for card in cards:
     print("-------------------------")
     # print(card)
     c = json.loads(requests.request("GET", "https://api.trello.com/1/cards/"+card["id"], params=param).text)
     actions = json.loads(requests.request("GET", "https://api.trello.com/1/cards/"+card["id"]+"/actions", params=param).text)
     # print("Nom de la carte : ", c["name"])
-    l_titres.append(c["name"])
+    name = c["name"]
+    d = {}
+    d["carte"] = Carte()
+    d[name].from_card(c)
+    d["actions"] = []
     # print("Description : ", c["desc"])
-    l_descr.append(c["desc"])
+    # l_descr.append(c["desc"])
     # print("action : ", actions)
     for action in actions:
         # print(action)
@@ -41,16 +82,18 @@ for card in cards:
         if "text" in action["data"]:
             # print("action : ", action["data"]["text"])
             # print("date : ", action["date"])
-            l_dates.append(action["date"])
-            l_textes.append(action["data"]["text"])
+            act = Action()
+            act.from_action(action)
+            d["actions"].append(act)
         # else:
         #     print(action)
 # response = requests.request("GET", url, params=param)
 
 # print(response.text)
 
-print(l_titres)
-print(l_dates)
-print(l_textes)
-print(l_descr)
+# print(l_titres)
+# print(l_dates)
+# print(l_textes)
+# print(l_descr)
+
 
